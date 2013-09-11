8726d07afb911014feb71bd15a38b9d92dbcb63b
Yuri F Becker <yurifbeck@gmail.com>
2013-09-11 11:09:28 -0400

    Tutorial 86 completed

--- Yuri/src/com/example/yuri/Tabs.java
+++ Yuri/src/com/example/yuri/Tabs.java
@@ -2,11 +2,16 @@ package com.example.yuri;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TabHost;
 import android.widget.TabHost.TabSpec;
+import android.widget.TextView;
 
-public class Tabs extends Activity {
-
+public class Tabs extends Activity implements OnClickListener{
+	//Criar fora do onCreate para ser usado por tudo
+	TabHost th;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -14,7 +19,13 @@ public class Tabs extends Activity {
 		setContentView(R.layout.tabs);
 
 		// Criar tabhost e endereçar ele para o xml
-		TabHost th = (TabHost) findViewById(R.id.tabhost);
+		th = (TabHost) findViewById(R.id.tabhost);
+		//Criar botoes
+		Button newTab = (Button)findViewById(R.id.bAddTab);
+		Button bStart = (Button)findViewById(R.id.bStartWatch);
+		Button bStop = (Button)findViewById(R.id.bStopWatch);
+		//Configurar "ouvidor" de toque na tela
+		newTab.setOnClickListener(this);
 		// Configurar tabhost
 		th.setup();
 		// Configurar o que é para ser mostrado
@@ -43,6 +54,40 @@ public class Tabs extends Activity {
 		specs.setIndicator("Add a Tab");
 		// Mostrar
 		th.addTab(specs);
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch(v.getId()){
+		case R.id.bAddTab:
+			//Ao se clicar no botao "bAddTab" uma nova aba é criada
+			TabSpec ourSpec = th.newTabSpec("tag1");
+			ourSpec.setContent(new TabHost.TabContentFactory() {
+				//Conteudo da nova aba é:
+				@Override
+				public View createTabContent(String tag) {
+					// TODO Auto-generated method stub
+					//Criar um texto
+					TextView text = new TextView(Tabs.this);
+					text.setText("You've created a new tab");
+					
+					return (text);
+				}
+			});
+			//Fazer a nova aba "ourSpec" aparecer
+			ourSpec.setIndicator("New");
+			th.addTab(ourSpec);
+			
+			break;
+		case R.id.bStartWatch:
+			
+			break;
+		case R.id.bStopWatch:
+			
+			break;
+		}
+		
 	}
 
 }
