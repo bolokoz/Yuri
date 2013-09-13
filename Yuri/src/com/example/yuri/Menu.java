94aa010c958bd5366029f7858666c222943f031d
Yuri F Becker <yurifbeck@gmail.com>
2013-09-13 00:12:51 -0400

    Tutorial 94 finished

--- Yuri/src/com/example/yuri/Menu.java
+++ Yuri/src/com/example/yuri/Menu.java
@@ -14,7 +14,7 @@ import android.widget.ListView;
 public class Menu extends ListActivity {
 
 	String classes[] = { "MainActivity", "TextPlay", "Email", "Camera", "Data",
-			"GFX", "GFXSurface", "SoundStuff", "Slider", "Tabs", "SimpleBrowser" };
+			"GFX", "GFXSurface", "SoundStuff", "Slider", "Tabs", "SimpleBrowser", "Flipper" };
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
